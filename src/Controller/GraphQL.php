f5af80c99b74a638834b47727344dad0cf306cc8
Alex <yuriev200@gmail.com>
2025-01-30 15:26:47 +0200

    root functionality

--- src/Controller/GraphQL.php
+++ src/Controller/GraphQL.php
@@ -2,8 +2,45 @@
 
 namespace App\Controller;
 
-class GraphQL {
-    public static function index() {
-        return json_encode(['success' => true]);
+use GraphQL\GraphQL as GraphQLCore;
+use GraphQL\Type\Schema;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+class GraphQL
+{
+    public static function index()
+    {
+
+        $queryType = new ObjectType([
+            'name' => 'Query',
+            'fields' => [
+                'hello' => [
+                    'type' => Type::string(),
+                    'resolve' => function () {
+                        return 'Hello, GraphQL!';
+                    }
+                ]
+            ]
+        ]);
+
+        $schema = new Schema([
+            'query' => $queryType,
+        ]);
+
+        try {
+            $rawInput = file_get_contents('php://input');
+            $input = json_decode($rawInput, true);
+            $query = $input['query'] ?? '';
+            $variables = $input['variables'] ?? null;
+
+            $result = GraphQLCore::executeQuery($schema, $query, null, null, $variables);
+            $output = $result->toArray();
+        } catch (\Exception $e) {
+            $output = ['error' => $e->getMessage()];
+        }
+
+        header('Content-Type: application/json');
+        return json_encode($output);
     }
 }
